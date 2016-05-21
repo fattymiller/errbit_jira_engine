2422998a765b14d2da81f72888d574fe46e3c37f
Ben Miller <ben.miller@cohortsolutions.com>
2016-05-22 08:01:12 +1000

    module for Jira

--- app/controllers/errbit_jira_engine/jira/autentication_controller.rb
+++ app/controllers/errbit_jira_engine/jira/autentication_controller.rb
@@ -1,51 +1,53 @@
-class Jira::AuthenticationController < ApplicationController
-  before_action { session[:jira] ||= {} }
-  
-  def initiate
-    request_token = jira_client.request_token(oauth_callback: jira.jira_finalise_url)
-    session[:jira][:request_token] = request_token.token
-    session[:jira][:request_token_secret] = request_token.secret
+module Jira
+  class AuthenticationController < ApplicationController
+    before_action { session[:jira] ||= {} }
     
-    redirect_to request_token.authorize_url
-  end
-  
-  def finalise
-    access_token = request_token.init_access_token(oauth_verifier: params[:oauth_verifier])
-    current_user.jira_token = access_token.token
-    current_user.jira_secret = access_token.secret
-    current_user.save!
-    
-    if session[:jira] && (problem_id = session[:jira][:create_issue])
-      problem = Problem.find(problem_id)
-      issue = IssueService.create_issue(self, problem, current_user) if problem
+    def initiate
+      request_token = jira_client.request_token(oauth_callback: jira.jira_finalise_url)
+      session[:jira][:request_token] = request_token.token
+      session[:jira][:request_token_secret] = request_token.secret
       
-      if issue
-        flash[:error] = issue.errors.full_messages.join(', ') unless issue.valid?
-        return redirect_to(main_app.app_problem_path(problem.app, problem))
-      end
+      redirect_to request_token.authorize_url
     end
+    
+    def finalise
+      access_token = request_token.init_access_token(oauth_verifier: params[:oauth_verifier])
+      current_user.jira_token = access_token.token
+      current_user.jira_secret = access_token.secret
+      current_user.save!
+      
+      if session[:jira] && (problem_id = session[:jira][:create_issue])
+        problem = Problem.find(problem_id)
+        issue = IssueService.create_issue(self, problem, current_user) if problem
+        
+        if issue
+          flash[:error] = issue.errors.full_messages.join(', ') unless issue.valid?
+          return redirect_to(main_app.app_problem_path(problem.app, problem))
+        end
+      end
 
-    redirect user_path(current_user)
-  ensure
-    session[:jira] = nil
-  end
-  
-  private
-  
-  def jira_client
-    @jira_client ||= ErrbitJiraEngine.create_client
-  end
-  
-  def request_token
-    return @request_token if @request_token
+      redirect user_path(current_user)
+    ensure
+      session[:jira] = nil
+    end
     
-    token = session[:jira][:request_token]
-    secret = session[:jira][:request_token_secret]
+    private
     
-    if token.present? && secret.present?
-      @request_token = jira_client.set_request_token(token, secret)
-    else
-      redirect_to jira.jira_initiate_path
+    def jira_client
+      @jira_client ||= ErrbitJiraEngine.create_client
+    end
+    
+    def request_token
+      return @request_token if @request_token
+      
+      token = session[:jira][:request_token]
+      secret = session[:jira][:request_token_secret]
+      
+      if token.present? && secret.present?
+        @request_token = jira_client.set_request_token(token, secret)
+      else
+        redirect_to jira.jira_initiate_path
+      end
     end
   end
 end
