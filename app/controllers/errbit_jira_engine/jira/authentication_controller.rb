6db5c658690f0c5804143d9b00c2529c5a992191
Ben Miller <ben.miller@cohortsolutions.com>
2016-05-23 07:07:16 +1000

    logging

--- app/controllers/errbit_jira_engine/jira/authentication_controller.rb
+++ app/controllers/errbit_jira_engine/jira/authentication_controller.rb
@@ -16,6 +16,8 @@ module Jira
       return unless request_token
       
       access_token = request_token.get_access_token(oauth_verifier: params[:oauth_verifier])
+      Rails.logger.info(access_token)
+      
       current_user.jira_token = access_token.token
       current_user.jira_secret = access_token.secret
       current_user.save!
