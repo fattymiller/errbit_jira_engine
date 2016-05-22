ae0464fd6969548b0cb5229703b999e647dba06b
Ben Miller <ben.miller@cohortsolutions.com>
2016-05-22 22:13:24 +1000

    ensure authoirsation token are still valid in a before_action call

--- app/controllers/errbit_jira_engine/problems_controller_concern.rb
+++ app/controllers/errbit_jira_engine/problems_controller_concern.rb
@@ -2,19 +2,20 @@ require 'active_support/concern'
 
 module ErrbitJiraEngine::ProblemsControllerConcern
   extend ActiveSupport::Concern
+  
+  included do
+    before_action :ensure_authorised!, only: :create_issue    
+  end
+  
+  private def ensure_authorised!
+    binding.pry
+    issue_tracker = app.issue_tracker.try(:type).try(:object)
+  
+    if issue_tracker && issue_tracker <= ErrbitJiraEngine::IssueTracker && !current_user.jira_authorisation_current?
+      session[:jira] ||= {}
+      session[:jira][:create_issue] = problem.id
 
-  def create_issue
-    issue_tracker = app.issue_tracker
-  
-    if issue_tracker && issue_tracker.is_a?(ErrbitJiraEngine::IssueTracker)
-      unless current_user.jira_authorisation_current?
-        session[:jira] ||= {}
-        session[:jira][:create_issue] = problem.id
-  
-        return redirect_to(jira.jira_initiate)
-      end
+      return redirect_to(jira.jira_initiate)
     end
-    
-    super
   end
 end
